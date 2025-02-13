b10bcbec659e1fc70de3dd338403e5a9c593a195
doston <raximoffdoston@gmail.com>
2025-02-14 02:14:50 +0500

    save

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import logo from "../assets/Logo.svg";
 
 export default function Navbar({ setSearchQuery }) {
   const [localSearchQuery, setLocalSearchQuery] = useState("");
-  const { cart } = useCart(); // Savatcha ma'lumotlarini olish
+  const { cart } = useCart();
 
   const handleSearch = () => {
     setSearchQuery(localSearchQuery);
@@ -19,7 +19,6 @@ export default function Navbar({ setSearchQuery }) {
 
   return (
     <nav className="w-full max-w-[1230px] flex flex-col sm:flex-row justify-between items-center m-auto mt-4 bg-white px-4 sm:px-6 lg:px-8 py-4 shadow-md mb-[20px]">
-      {/* Logo */}
       <div className="flex items-center justify-between w-full sm:w-auto">
         <img src={logo} alt="Logo" className="h-[40px] sm:h-[50px] md:h-[60px]" />
         <button className="sm:hidden w-[40px] h-[40px] bg-[#4d5ef6] text-white rounded-[8px] flex items-center justify-center text-[24px]">
@@ -27,7 +26,6 @@ export default function Navbar({ setSearchQuery }) {
         </button>
       </div>
 
-      {/* Search and Buttons */}
       <div className="flex items-center justify-between gap-4 w-full sm:w-auto mt-4 sm:mt-0">
         <div className="flex items-center gap-2 sm:gap-4 w-full max-w-[500px]">
           <input
@@ -50,7 +48,6 @@ export default function Navbar({ setSearchQuery }) {
             <FaRegHeart />
           </button>
 
-          {/* Savatcha tugmasi */}
           <Link to="/cart" className="relative">
             <button className="w-[40px] h-[40px] sm:w-[46px] sm:h-[46px] bg-[#e4e7ee] text-[#0d63f3] rounded-[8px] text-[20px] sm:text-[24px] p-auto flex items-center justify-center gap-[5px] cursor-pointer">
               <MdOutlineShoppingCart />
@@ -64,7 +61,6 @@ export default function Navbar({ setSearchQuery }) {
         </div>
       </div>
 
-      {/* Desktop Navigation Menu */}
       <div className="hidden sm:flex items-center justify-center gap-6 mt-4 sm:mt-0">
         <button className="w-[149px] h-[46px] bg-gradient-to-r from-[#4d5ef6] to-[#f64d4d] flex items-center justify-center text-white rounded-[8px] text-base">
           <IoMdMenu className="text-[24px]" /> KATALOG
